9a3fbea5e0d7246298fee97858d9ea2e57e5b8a3
Fraser Sabine <frasersab@outlook.com>
2019-11-24 13:28:27 +1100

    changed to mathjs

--- src/JellyBrain.js
+++ src/JellyBrain.js
@@ -1,5 +1,6 @@
-const Matrix = require('./Matrix.js')
+const math = require('mathjs')
 
+// activation function class
 class ActivationFunction {
     constructor(func, dfunc) {
         this.func = func;
@@ -7,49 +8,63 @@ class ActivationFunction {
     }
 }
 
+let lrelu = new ActivationFunction(
+    x => { if (x < 0) { return 0.1 * x } else { return x } },
+    y => { if (y < 0) { return 0.1 } else { return 1 } }
+);
+
+let relu = new ActivationFunction(
+    x => { if (x < 0) { return 0 } else { return x } },
+    y => { if (y <= 0) { return 0 } else { return 1 } }
+);
+
 let sigmoid = new ActivationFunction(
-    x => 1 / (1 + Math.exp(-x)),
+    x => 1 / (1 + math.exp(-x)),
     y => y * (1 - y)
 );
 
 let tanh = new ActivationFunction(
-    x => Math.tanh(x),
+    x => math.tanh(x),
     y => 1 - (y * y)
 );
 
 class JellyBrain {
-    constructor(inputNodes, hiddenNodes, outputNodes, learningRate = 0.1, activationFunction = sigmoid) {
+    constructor(inputNodes, hiddenNodes, outputNodes, learningRate = 0.1, activationFunction = relu) {
+        // set the parameteres for the neural network
         this.inputNodes = inputNodes;
         this.hiddenNodes = hiddenNodes;
         this.outputNodes = outputNodes;
         this.learningRate = learningRate;
         this.activationFunction = activationFunction;
 
-        this.weightsIH = new Matrix(hiddenNodes, inputNodes);
-        this.weightsHO = new Matrix(outputNodes, hiddenNodes);
-        this.weightsIH.randomise();
-        this.weightsHO.randomise();
+        // find the optimal range to initialise the weights to prevent saturation
+        let initIHRange = 1 / math.sqrt(this.hiddenNodes);
+        let initHORange = 1 / math.sqrt(this.outputNodes);
 
-        this.biasHidden = new Matrix(this.hiddenNodes, 1);
-        this.biasOutput = new Matrix(this.outputNodes, 1);
-        this.biasHidden.randomise();
-        this.biasOutput.randomise();
+        // create the weights matrix with random weights within the specified range
+        this.weightsIH = math.random([this.hiddenNodes, this.inputNodes], -initIHRange, initIHRange);
+        this.weightsHO = math.random([this.outputNodes, this.hiddenNodes], -initHORange, initHORange);
+
+        // create bias arrays with initialisation to 0
+        this.biasHidden = math.zeros([1, this.hiddenNodes]);
+        this.biasOutput = math.zeros([1, outputNodes]);
+
     }
 
     guess(inputs) {
-        // Generate hidden layer
-        let intputArray = Matrix.fromArray(inputs);
-        let hiddenArray = Matrix.multiply(this.weightsIH, intputArray);
-        hiddenArray.add(this.biasHidden);
-        hiddenArray.map(this.activationFunction.func);
+        // // Generate hidden layer
+        // let intputArray = Matrix.fromArray(inputs);
+        // let hiddenArray = Matrix.multiply(this.weightsIH, intputArray);
+        // hiddenArray.add(this.biasHidden);
+        // hiddenArray.map(this.activationFunction.func);
 
-        // Generate outputs
-        let outputs = Matrix.multiply(this.weightsHO, hiddenArray);
-        outputs.add(this.biasOutput);
-        outputs.map(this.activationFunction.func);
+        // // Generate outputs
+        // let outputs = Matrix.multiply(this.weightsHO, hiddenArray);
+        // outputs.add(this.biasOutput);
+        // outputs.map(this.activationFunction.func);
 
-        // send back array of outputs
-        return outputs.toArray();
+        // // send back array of outputs
+        // return outputs.toArray();
     }
     train() {
 
@@ -61,8 +76,10 @@ class JellyBrain {
     }
 }
 
-const matrix = math.matrix([[7, 1], [-2, 3]])
-
-console.log(matrix);
 test = new JellyBrain(2, 4, 2);
-console.log(test.guess([0, 1]));
+
+console.log(relu.dfunc(3));
+// let ran = 1 / math.sqrt(4)
+// console.log(ran);
+// let matrix = math.random([4, 4], -ran, ran);
+// console.table(matrix);
