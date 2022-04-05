21b1da743a73e22f5851ba1122b4229f93651dba
Fraser Sabine <frasersab@outlook.com>
2022-04-05 23:26:19 +1000

    mnist reader into seperate read and save functions

--- src/mnist_dataset/mnist_reader.cjs
+++ src/mnist_dataset/mnist_reader.cjs
@@ -1,32 +1,55 @@
 var fs = require('fs');
-const { createCanvas, loadImage } = require('canvas');
-const { ctransposeDependencies } = require('mathjs');
+const {createCanvas} = require('canvas');
 
-var dataFileBuffer = fs.readFileSync('src\\mnist_dataset\\test_images_10k.idx3-ubyte');
-var labelFileBuffer = fs.readFileSync('src\\mnist_dataset\\test_labels_10k.idx1-ubyte');
-const canvas = createCanvas(28, 28)
-const ctx = canvas.getContext('2d')
-var pixelValues = [];
-
-
-for (var image = 0; image < 3; image++) { 
-    var pixels = [];
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (var y = 0; y <= 27; y++) {
-        for (var x = 0; x <= 27; x++) {
-            var pixel = dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 16];
-            pixels.push(pixel);
-            var colour = 255 - pixel;
-            ctx.fillStyle = `rgb(${colour}, ${colour}, ${colour})`;
-            ctx.fillRect(x, y, 1, 1);
+function readMNIST(start, end)
+{
+    var dataFileBuffer = fs.readFileSync(__dirname + '\\test_images_10k.idx3-ubyte');
+    var labelFileBuffer = fs.readFileSync(__dirname + '\\test_labels_10k.idx1-ubyte');
+    var pixelValues = [];
+    
+    for (var image = start; image < end; image++)
+    { 
+        var pixels = [];
+        for (var y = 0; y <= 27; y++)
+        {
+            for (var x = 0; x <= 27; x++)
+            {
+                pixels.push(dataFileBuffer[(image * 28 * 28) + (x + (y * 28)) + 16]);
+            }
         }
+
+        var imageData  = {};
+        imageData["index"] = image;
+        imageData["label"] = labelFileBuffer[image + 8];
+        imageData["pixels"] = pixels;
+        pixelValues.push(imageData);
     }
-
-    var label = labelFileBuffer[image + 8];
-    var imageData  = {};
-    imageData[JSON.stringify(label)] = pixels;
-    pixelValues.push(imageData);
-
-    const buffer = canvas.toBuffer('image/png')
-    fs.writeFileSync(`./image${image}-${label}.png`, buffer)
+    return pixelValues;
 }
+
+function saveMNIST(start, end)
+{
+    const canvas = createCanvas(28, 28);
+    const ctx = canvas.getContext('2d');
+
+    var pixelValues = readMNIST(start, end);
+
+    pixelValues.forEach(function(image)
+    { 
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        for (var y = 0; y <= 27; y++)
+        {
+            for (var x = 0; x <= 27; x++)
+            {
+                var pixel = image.pixels[x + (y * 28)];
+                var colour = 255 - pixel;
+                ctx.fillStyle = `rgb(${colour}, ${colour}, ${colour})`;
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
+        const buffer = canvas.toBuffer('image/png')
+        fs.writeFileSync(__dirname + `\\images\\image${image.index}-${image.label}.png`, buffer)
+    })
+}
+
+saveMNIST(0, 5);
