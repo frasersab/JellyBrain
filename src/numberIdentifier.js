74a6c7a634764fb2cf81137464cc99c11efa342b
Fraser Sabine <frasersab@outlook.com>
2023-06-23 21:18:50 +1000

    Added progress bar

--- src/numberIdentifier.js
+++ src/numberIdentifier.js
@@ -1,6 +1,7 @@
-const math = require('mathjs')
-const {JellyBrain, linear} = require('./JellyBrain.js')
-const {readMNIST, saveMNIST} = require('./mnist_dataset/mnist_reader')
+const math = require('mathjs');
+const {JellyBrain, linear} = require('./JellyBrain.js');
+const {readMNIST, saveMNIST} = require('./mnist_dataset/mnist_reader');
+const cliProgress = require('cli-progress');
 
 
 let brain = new JellyBrain(784, 784, 10);
@@ -8,18 +9,24 @@ let brain = new JellyBrain(784, 784, 10);
 // training function
 function trainer(brain, amount) {
     var pixelValues = readMNIST(0, amount, '\\train_images_60k.idx3-ubyte', '\\train_labels_60k.idx1-ubyte');
-
+    const progressBar = new cliProgress.SingleBar({format: 'Training Progress |' + '{bar}' + '| {percentage}% | {value}/{total} | ETA: {eta}s'}, cliProgress.Presets.shades_classic);
+    progressBar.start(amount, 0);
+    
     pixelValues.forEach(function(image)
     {
         targetArray = new Array(10).fill(0);
         targetArray[image.label] = 1;
         brain.train(image.pixels, targetArray);
+        progressBar.increment();
     })
+    progressBar.stop();
 }
 
 // testing function
 function tester(brain, amount) {
     var pixelValues = readMNIST(0, amount, '\\test_images_10k.idx3-ubyte', '\\test_labels_10k.idx1-ubyte');
+    const progressBar = new cliProgress.SingleBar({format: 'Testing Progress |' + '{bar}' + '| {percentage}% | {value}/{total} | ETA: {eta}s'}, cliProgress.Presets.shades_classic);
+    progressBar.start(amount, 0);
     let accuracy = 0;
     pixelValues.forEach(function(image)
     {
@@ -31,12 +38,14 @@ function tester(brain, amount) {
         if (guessNumber == image.label){
             accuracy++;
         }
+        progressBar.increment();
     })
 
+    progressBar.stop();
     return accuracy = (accuracy / amount) * 100;
 }
 
-let trainAmount = 100;
+let trainAmount = 50;
 let testAmount = 20;
 
 let accuracyTable = Array();
