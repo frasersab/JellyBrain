0e73226ec7043430140b2d23b0408e355ef9df24
Fraser Sabine <frasersab@outlook.com>
2023-08-06 10:48:51 +1000

    updated paths and created some new brains

--- src/test/numberIdentifier.js
+++ src/test/numberIdentifier.js
@@ -1,30 +1,32 @@
 const math = require('mathjs');
+const path = require('path');
 const {JellyBrain, costFuncs, activationFuncs} = require('../JellyBrain.js');
 const {readMNIST, saveMNIST} = require('../mnist_dataset/mnist_reader.js');
 const fs = require('fs');
 const cliProgress = require('cli-progress');
 
 
-let brain = new JellyBrain(784, 784, 10, costFuncs.crossEntropy, 0.01, activationFuncs.sigmoid, activationFuncs.softmax);
+let brain = new JellyBrain(784, 784, 10, costFuncs.crossEntropy, 0.0005, activationFuncs.sigmoid, activationFuncs.softmax);
 
 // save brain
 function saveBrain(brain, name)
 {
     let contents = JSON.stringify(brain.exportBrain());
-    fs.writeFileSync(__dirname + `\\brains\\${name}.json`, contents);
+    fs.writeFileSync(path.join(__dirname, '..'
+    , 'brains', `${name}.json`), contents);
 }
 
 // load brain
 function loadBrain(brain, name)
 {
-    let contents = fs.readFileSync(__dirname + `\\brains\\${name}.json`);
+    let contents = fs.readFileSync(path.join(__dirname, '..', 'brains', `${name}.json`));
     contents = JSON.parse(contents);
     brain.importBrain(contents);
 }
 
 // training function
-function trainer(brain, amount) {
-    var pixelValues = readMNIST(0, amount, '\\train_images_60k.idx3-ubyte', '\\train_labels_60k.idx1-ubyte', true);
+function trainer(brain, amount, start = 0) {
+    var pixelValues = readMNIST(start, start + amount, '\\train_images_60k.idx3-ubyte', '\\train_labels_60k.idx1-ubyte', true);
     const progressBar = new cliProgress.SingleBar({format: 'Training Progress |' + '{bar}' + '| {percentage}% | {value}/{total} | ETA: {eta}s'}, cliProgress.Presets.shades_classic);
     progressBar.start(amount, 0);
     
@@ -39,8 +41,8 @@ function trainer(brain, amount) {
 }
 
 // testing function
-function tester(brain, amount) {
-    var pixelValues = readMNIST(0, amount, '\\test_images_10k.idx3-ubyte', '\\test_labels_10k.idx1-ubyte', true);
+function tester(brain, amount, start = 0) {
+    var pixelValues = readMNIST(start, start + amount, '\\test_images_10k.idx3-ubyte', '\\test_labels_10k.idx1-ubyte', true);
     const progressBar = new cliProgress.SingleBar({format: 'Testing Progress |' + '{bar}' + '| {percentage}% | {value}/{total} | ETA: {eta}s'}, cliProgress.Presets.shades_classic);
     progressBar.start(amount, 0);
     let accuracy = 0;
@@ -61,20 +63,20 @@ function tester(brain, amount) {
     return accuracy = (accuracy / amount) * 100;
 }
 
+let startFrom = 21000;
+let trainAmount = 1000;
+let testAmount = 2000;
 
-let trainAmount = 2000;
-let testAmount = 100;
-
-saveBrain(brain, "brain_before");
-//loadBrain(brain, "brain_after")
+//saveBrain(brain, "brain_before");
+loadBrain(brain, "brain21000")
 
 let accuracyTable = Array();
 accuracyTable.push(["Training Samples", "Accuracy"]);
 
 accuracyTable.push([0, tester(brain, testAmount) + "%"]);
-trainer(brain, trainAmount);
-accuracyTable.push([trainAmount, tester(brain, testAmount) + "%"]);
+//trainer(brain, trainAmount, startFrom);
+//accuracyTable.push([trainAmount, tester(brain, testAmount) + "%"]);
 
-saveBrain(brain, "brain_after");
+saveBrain(brain, "brain22000");
 
 console.table(accuracyTable);
