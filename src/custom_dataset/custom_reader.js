3242901d401119c26317f7dd3f8d17c07b3bae49
Fraser Sabine <frasersab@outlook.com>
2024-05-03 09:17:53 -0500

    updated custom dataset functions

--- src/custom_dataset/custom_reader.js
+++ src/custom_dataset/custom_reader.js
@@ -1,33 +1,41 @@
 var fs = require('fs');
-var imageData = require('./custom_images.json');
+const imagesData = require('./custom_images.json');
 const {createCanvas} = require('canvas');
 
+function readCustom(start, end)
+{
+    return imagesData.slice(start, end);
+}
 
-function saveCustom(index)
+function saveCustom(start, end)
 {
     const canvas = createCanvas(28, 28);
     const ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    pixels = imageData[index].pixels
-    label = imageData[index].label
+    imagesDataSlice = readCustom(start, end);
 
-    for (var y = 0; y <= 27; y++)
+    imagesDataSlice.forEach(function(image)
     {
-        for (var x = 0; x <= 27; x++)
-        {
-            var pixel = pixels[x + (y * 28)];
-            var colour = 255 - pixel;
-            ctx.fillStyle = `rgb(${colour}, ${colour}, ${colour})`;
-            ctx.fillRect(x, y, 1, 1);
-        }
-    }
-    const buffer = canvas.toBuffer('image/png')
-    fs.writeFileSync(__dirname + `\\images\\image${index}-${label}.png`, buffer)
+        pixels = image.pixels
+        label = image.label
 
+        for (var y = 0; y <= 27; y++)
+        {
+            for (var x = 0; x <= 27; x++)
+            {
+                var pixel = pixels[x + (y * 28)];
+                var colour = 255 - pixel;
+                ctx.fillStyle = `rgb(${colour}, ${colour}, ${colour})`;
+                ctx.fillRect(x, y, 1, 1);
+            }
+        }
+        const buffer = canvas.toBuffer('image/png')
+        fs.writeFileSync(__dirname + `\\images\\image${image.index}-${label}.png`, buffer)
+    })
 }
 
-saveCustom(6);
+saveCustom(0, 6);
 
-//exports.readCustom = readCustom
+exports.readCustom = readCustom
 exports.saveCustom = saveCustom
