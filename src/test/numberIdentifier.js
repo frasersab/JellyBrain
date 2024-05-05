2fccb09f12af4061eaaa6eca728e2dbc076d56f3
Fraser Sabine <frasersab@outlook.com>
2024-05-04 21:03:55 -0500

    added possibility to train from custom set

--- src/test/numberIdentifier.js
+++ src/test/numberIdentifier.js
@@ -2,10 +2,11 @@ const math = require('mathjs');
 const path = require('path');
 const {JellyBrain, costFuncs, activationFuncs} = require('../JellyBrain.js');
 const {readMNIST, saveMNIST} = require('../mnist_dataset/mnist_reader.js');
+const {readCustom, saveCustom} = require('../custom_dataset/custom_reader.js');
 const fs = require('fs');
 const cliProgress = require('cli-progress');
 
-
+// create brain for mnist dataset
 let numberBrain = new JellyBrain(784, 784, 10, costFuncs.crossEntropy, 0.0008, activationFuncs.sigmoid, activationFuncs.softmax);
 
 // save brain
@@ -26,14 +27,24 @@ function loadBrain(brain, name)
 
 
 // training function
-function trainer(brain, numberOfBatches, batchSize = 1, start = 0) {
+function trainer(brain, numberOfBatches, batchSize = 1, start = 0, customDataset = false) {
     const progressBar = new cliProgress.SingleBar({format: 'Training Progress |' + '{bar}' + '| {percentage}% | {value}/{total} | ETA: {eta}s'}, cliProgress.Presets.shades_classic);
     progressBar.start(numberOfBatches * batchSize, 0);
 
     for (let batchNumber = 0; batchNumber < numberOfBatches; batchNumber++)
     {
         let batchStart = start + (batchNumber * batchSize);
-        let imagesData = readMNIST(batchStart, batchStart + batchSize, '\\train_images_60k.idx3-ubyte', '\\train_labels_60k.idx1-ubyte', true);
+        let imagesData;
+        if (customDataset)
+        {
+            imagesData = readCustom(batchStart, batchStart + batchSize);
+        }
+        else
+        {
+            imagesData = readMNIST(batchStart, batchStart + batchSize, '\\train_images_60k.idx3-ubyte', '\\train_labels_60k.idx1-ubyte', true);
+        }
+        
+
         imagesData.forEach(function(image)
         {
             targetArray = new Array(10).fill(0);
@@ -47,8 +58,16 @@ function trainer(brain, numberOfBatches, batchSize = 1, start = 0) {
 }
 
 // testing function
-function tester(brain, amount, start = 0) {
-    var imagesData = readMNIST(start, start + amount, '\\test_images_10k.idx3-ubyte', '\\test_labels_10k.idx1-ubyte', true);
+function tester(brain, amount, start = 0, customDataset = false) {
+    var imagesData;
+    if (customDataset)
+    {
+        imagesData = readCustom(batchStart, start + amount);
+    }
+    else
+    {
+        imagesData = readMNIST(start, start + amount, '\\test_images_10k.idx3-ubyte', '\\test_labels_10k.idx1-ubyte', true);
+    }
     const progressBar = new cliProgress.SingleBar({format: 'Testing Progress |' + '{bar}' + '| {percentage}% | {value}/{total} | ETA: {eta}s'}, cliProgress.Presets.shades_classic);
     progressBar.start(amount, 0);
     let accuracy = 0;
@@ -69,21 +88,21 @@ function tester(brain, amount, start = 0) {
     return accuracy = (accuracy / amount) * 100;
 }
 
-let numberOfBatches = 28000;
+let numberOfBatches = 50;
 let batchSize = 1;
-let startFrom = 32000;
-let testAmount = 10000;
+let startFrom = 0;
+let testAmount = 2000;
 
 //saveBrain(numberBrain, "brain_before");
-loadBrain(numberBrain, "brain32000")
+loadBrain(numberBrain, "brain60000")
 
 
 let accuracyTable = Array();
 accuracyTable.push(["Training Samples", "Accuracy"]);
-//accuracyTable.push([0, tester(numberBrain, testAmount) + "%"]);
+accuracyTable.push([0, tester(numberBrain, testAmount) + "%"]);
 trainer(numberBrain, numberOfBatches, batchSize, startFrom);
 accuracyTable.push([numberOfBatches * batchSize, tester(numberBrain, testAmount) + "%"]);
 
-saveBrain(numberBrain, "brainBatchTest");
+saveBrain(numberBrain, "brainPostTraining");
 
 console.table(accuracyTable);
